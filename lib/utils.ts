53969e2306cd6587b1c2cacee02d246c7baa205d
Hamed Bahram <hamed.bahram@gmail.com>
2024-06-03 16:44:21 -0400

    Remove placeholder import in utils file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,4 @@
 import { type ClassValue, clsx } from 'clsx'
-import { getPlaiceholder } from 'plaiceholder'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
